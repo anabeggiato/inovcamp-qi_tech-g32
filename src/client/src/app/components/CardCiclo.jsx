7c5e15933a71972734e967f766eb7b565be3a823
Ana Beatriz Passos Beggiato <ana.beggiato@sou.inteli.edu.br>
2025-09-30 04:03:14 -0300

    feat: adiciona seção de tecnologias à landing page

--- src/client/src/app/components/CardCiclo.jsx
+++ src/client/src/app/components/CardCiclo.jsx
@@ -1,8 +1,9 @@
 
 
-export default function CardCiclo({ icon, title, description }) {
+export default function CardCiclo({ icon, title, description, align = "center" }) {
     return (
-        <div className="border-2 border-gray-200 rounded-lg p-6 max-w-[90%] hover:shadow-lg">
+        <div className={`flex flex-col border-2 border-gray-200 rounded-lg p-6 max-w-[90%] hover:shadow-lg 
+        ${align === "center" ? "items-center text-center" : "items-start text-left"}`}>
             <div className="w-12 h-12 bg-purple-gradient rounded-lg flex items-center justify-center mb-4">
                 {icon}
             </div>
